385323da63dc8546bb17d77452bb70bd6fb348ba
Conor Hamilton <conhamil19@gmail.com>
2018-05-22 05:50:45 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,15 @@
 class Dog 
-  attr_accessor :name
+  attr_reader :name
+  @@all = []
+  
+  def initialize(name)
+    @name = name 
+    @all << self
+  end
+  
+  def self.all 
+    @@all.each do |a|
+      puts a 
+    end
+  end
 end
